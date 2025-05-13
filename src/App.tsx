0769920725171c2c16fc8fba95546f545d09f718
RodrLH <u202212338@upc.edu.pe>
2025-05-12 19:23:02 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ThemeProvider from "./components/theme/ThemeProvider";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Bonds from "./pages/Bonds";
+import NewBond from "./pages/NewBond";
+import EditBond from "./pages/EditBond";
+import BondDetail from "./pages/BondDetail";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <AuthProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Navigate to="/login" />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/bonds" element={<ProtectedRoute><Bonds /></ProtectedRoute>} />
+              <Route path="/bonds/new" element={<ProtectedRoute><NewBond /></ProtectedRoute>} />
+              <Route path="/bonds/:id" element={<ProtectedRoute><BondDetail /></ProtectedRoute>} />
+              <Route path="/bonds/:id/edit" element={<ProtectedRoute><EditBond /></ProtectedRoute>} />
+              <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+              <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </AuthProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
+
+export default App;
